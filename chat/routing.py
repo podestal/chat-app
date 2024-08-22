aaafda5d49678bfe5f74102c5cf977bcb37c21fc
podestal <luisrodriguezpodesta@gmail.com>
2024-08-22 13:47:26 -0400

    routing done

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
+from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    
+    path('ws/chat/<str:room_name>/', consumers.chatConsumers.as_asgi()),
 ]
